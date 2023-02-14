ed7dfec51dc4b2c53e0023af1ad2771ea8808fa0
Francesca Piccoli <>
2023-02-14 16:49:29 +0100

    feat: detail improved with UI

--- src/acr-cloud/pages/Detail.tsx
+++ src/acr-cloud/pages/Detail.tsx
@@ -1,6 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 import { RootStackParamList } from "./types";
 
@@ -12,9 +12,13 @@ export const Detail = ({
 }: NativeStackScreenProps<RootStackParamList, "Detail">) => {
   return (
     <View style={styles.container}>
-      <Text>{title}</Text>
-      <Text>{album}</Text>
-      <Text>{artist}</Text>
+      <Image
+        style={styles.image}
+        source={require("../../../assets/album_test.jpg")}
+      />
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.album}>{album}</Text>
+      <Text style={styles.artist}>{artist}</Text>
     </View>
   );
 };
@@ -23,12 +27,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    padding: 20,
     alignItems: "center",
-    justifyContent: "center",
   },
-  button: {
-    width: 200,
-    backgroundColor: "blue",
-    color: "white",
+  image: {
+    height: undefined,
+    width: "100%",
+    aspectRatio: 1,
+    borderRadius: 20,
   },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginVertical: 8,
+  },
+  album: { fontSize: 18, color: "grey" },
+  artist: { fontSize: 18 },
 });
